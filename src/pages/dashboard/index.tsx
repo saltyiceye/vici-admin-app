42473504b19d135864d970430cf94090e38dfa25
smpp ye <dev007@kf.com>
2025-12-09 18:52:09 +0800

    add chart

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,6 @@
 
 import {SectionCards} from "./section-cards";
+import {ChartAreaInteractive} from "./chart-area-interactive";
 
 export default function Dashboard() {
   return (
@@ -8,6 +9,7 @@ export default function Dashboard() {
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
               <SectionCards />
               <div className="px-4 lg:px-6">
+                <ChartAreaInteractive />
               </div>
             </div>
           </div>
