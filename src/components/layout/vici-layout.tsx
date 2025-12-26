4360a8fea2ae64fbf76286a25f2936d29ecde836
smpp ye <dev007@kf.com>
2025-12-26 15:37:06 +0800

    sidebar bg

--- src/components/layout/vici-layout.tsx
+++ src/components/layout/vici-layout.tsx
@@ -38,20 +38,15 @@ export const ViciLayout = (props: { children: ReactNode }) => {
     >
       <AppSidebar variant="inset" />
       <SidebarInset>
-      <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
-            <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
-                <SidebarTrigger className="-ml-1" />
-                <Separator
-                    orientation="vertical"
-                    className="mx-2 data-[orientation=vertical]:h-4"
-                />
-
-                <div className="flex-1 flex items-center" id="breadcrumb" />
-                <LocalesMenuButton />
-                <ThemeModeToggle />
-                <RefreshButton />
-                <UserMenu />
-            </div>
+        <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
+          <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
+            <SidebarTrigger className="-ml-1" />
+            <div className="flex-1 flex items-center" id="breadcrumb" />
+            <LocalesMenuButton />
+            <ThemeModeToggle />
+            <RefreshButton />
+            <UserMenu />
+          </div>
         </header>
 
         {/* <SectionCards />
